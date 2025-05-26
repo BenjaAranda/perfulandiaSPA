e3e50ed8e21b31071ec56ae0eb37ca4a40b775e1
benja <be.arandaa@duocuc.cl>
2025-05-26 01:12:05 -0400

    Creando microservicio de ventas

---
+++ msvc-ventas/src/test/java/benja/msvc_ventas/MsvcVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package benja.msvc_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
